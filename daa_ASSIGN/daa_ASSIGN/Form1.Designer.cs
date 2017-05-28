19b3042ead31af2cf973af8460ee45d1fcbdb9e1
Muhammad Ejaz <muhammad.ejaz1996@gmail.com>
2017-05-28 23:13:51 +0500

    Comparision between quick sort and heap sort on winform

---
+++ daa_ASSIGN/daa_ASSIGN/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace daa_ASSIGN
+{
+    partial class daa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.draw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(37, 12);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "QuickSort";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "HeapSort";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(1205, 582);
+            this.chart.TabIndex = 2;
+            this.chart.Text = "chart1";
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(544, 600);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(75, 23);
+            this.draw.TabIndex = 1;
+            this.draw.Text = "Start Sorting";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // daa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1229, 635);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.draw);
+            this.Name = "daa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DAA_ASSIGNMENT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.daa_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button draw;
+    }
+}
+
---
+++ daa_ASSIGN/daa_ASSIGN/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace daa_ASSIGN
+{
+    public partial class daa : Form
+    {
+        public daa()
+        {
+            InitializeComponent();
+        }
+
+        private void daa_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int s = Partition(arr, left, right);
+                QuickSort(arr, left, s - 1);
+                QuickSort(arr, s + 1, right);
+            }
+        }
+        static int Partition(int[] arr, int first, int last)
+        {
+            int pivotvalue = arr[first];
+            int leftmark = first + 1;
+            int rightmark = last;
+            Boolean done = false;
+            while (!done)
+            {
+                while ((leftmark <= rightmark) && (arr[leftmark] <= pivotvalue))
+                    leftmark = leftmark + 1;
+                while ((arr[rightmark] >= pivotvalue) && (rightmark >= leftmark))
+                    rightmark = rightmark - 1;
+                if (rightmark < leftmark)
+                    done = true;
+                else
+                    swap(ref arr[leftmark], ref arr[rightmark]);
+            }
+            swap(ref arr[first], ref  arr[rightmark]);
+            return rightmark;
+        }
+        static void swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        static void HeapSort(int[] input)
+        {
+            //Build-Max-Heap
+            int heapSize = input.Length;
+            for (int p = (heapSize - 1) / 2; p >= 0; p--)
+                MaxHeapify(input, heapSize, p);
+            for (int i = input.Length - 1; i > 0; i--)
+            {
+                //Swap
+                swap(ref input[i], ref input[0]);
+                heapSize--;
+                MaxHeapify(input, heapSize, 0);
+            }
+        }
+        static void MaxHeapify(int[] input, int heapSize, int index)
+        {
+            int left = (index + 1) * 2 - 1;
+            int right = (index + 1) * 2;
+            int largest = 0;
+            if (left < heapSize && input[left] > input[index])
+                largest = left;
+            else
+                largest = index;
+
+            if (right < heapSize && input[right] > input[largest])
+                largest = right;
+
+            if (largest != index)
+            {
+                swap(ref input[index], ref input[largest]);
+                MaxHeapify(input, heapSize, largest);
+            }
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+
+            int size = 100;
+            Stopwatch sw = new Stopwatch();
+            Random rnd = new Random();
+            int[] arr;
+            int[] arr1;
+            int x;
+            int s1 = 4;
+            double[] QuickTime = new double[s1+1];
+            double[] HeapTime = new double[s1+1];
+            int[] xAxis = new int[s1+1];
+            for (int i = 0; i <= s1; i++)
+            {
+                xAxis[i] = size;
+                arr = new int[size];
+                arr1 = new int[size];
+                for (int j = 0; j < size; j++)
+                {
+                    x = rnd.Next(1, size);
+                    arr[j] = x;
+                    arr1[j] = x;
+                }
+                sw.Reset();
+                sw.Start();
+                QuickSort(arr, 0, size - 1);
+                sw.Stop();
+                QuickTime[i] = sw.Elapsed.TotalMilliseconds;
+                sw.Reset();
+                sw.Start();
+                HeapSort(arr1);
+                sw.Stop();
+                HeapTime[i] = sw.Elapsed.TotalMilliseconds;
+                size *= 10;
+            }
+            fillChart(QuickTime, HeapTime, xAxis);
+        }
+        void fillChart(double [] quick, double [] heap, int[] size)
+        {
+            chart.Titles.Add("Time Chart").Font = new Font("Arial", 16, FontStyle.Bold);
+            chart.ChartAreas[0].AxisX.Title = "Input";
+            chart.ChartAreas[0].AxisY.Title = "MilliSeconds";
+            chart.ChartAreas[0].AxisX.TitleFont = new Font("Arial", 16,FontStyle.Bold);
+            chart.ChartAreas[0].AxisY.TitleFont = new Font("Arial", 16, FontStyle.Bold);
+            chart.Series["QuickSort"].BorderWidth = 4;
+            chart.Series["HeapSort"].BorderWidth = 4;
+            chart.ChartAreas[0].AxisX.Minimum = 0;
+            for (int i = 0; i < heap.Length; i++)
+            {
+                MessageBox.Show(size[i].ToString()+"  "+quick[i]+"  "+heap[i]);
+
+                //chart.Series["QuickSort"].Points.AddXY(size[i], quick[i]);
+                //chart.Series["HeapSort"].Points.AddXY(size[i], heap[i]);
+            }
+        }
+       
+
+    }
+}
